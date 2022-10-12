2bce8d73959249ec1ab1afdd41748f529de695d0
Bruno <dronecraftbr10@gmail.com>
2022-10-11 23:05:45 -0300

    Realizando alteração no modelo de Venda

--- Models/VendaModel.cs
+++ Models/VendaModel.cs
@@ -3,7 +3,7 @@ namespace tech_test_payment_api.Models
     public class VendaModel
     {
         public int Id { get; set; }
-        public int VendedorId { get; set; }
+        public VendedorModel? Vendedor { get; set; }
         public DateTime Data { get; set; }
         public EnumStatus Status { get; set; }
         public List<string>? Itens { get; set; }
